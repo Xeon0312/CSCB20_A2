81ffc2c07aebb4e4d8631625d7060f276c08e696
Xeon0312 <caoboyu0312@gmail.com>
2020-03-15 22:17:58 -0400

    准备交作业第一版

---
+++ question1.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def default():
+    return "Welcome, to my CSCB20 website!"
+
+
+@app.route('/<name>')
+def welcome(name):
+    return "Welcome, " + updateStr(name) + ", to my CSCB20 website!"
+
+
+def updateStr(name):
+    output = ""
+    if name.isalpha():
+        for c in name:
+            output += c.swapcase()
+    else:
+        for c in name:
+            if c.isalpha():
+                output += c
+    return output
+
+
+if __name__ == '__main__':
+    # app.run()
+    app.run(debug=True)
